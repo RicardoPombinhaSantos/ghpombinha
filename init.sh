ca42f0b8a1a1681d71d951bc9d8cfb07bd2f5f95
RicardoPombinhaSantos <godhja@gmail.com>
2025-12-30 12:17:47 +0000

    Update remote repository URL in init.sh

--- init.sh
+++ init.sh
@@ -2,5 +2,5 @@ git init
 git add .
 git commit -m "primeira versão chatbot guesthouse"
 git branch -M main
-git remote add origin https://github.com/teu-utilizador/guesthouse-chatbot.git
+git remote add origin https://github.com/teu-utilizador/ghpombinha.git
 git push -u origin main
