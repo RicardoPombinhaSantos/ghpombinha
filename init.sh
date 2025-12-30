90267365dc769c013812b85ff38cefc4936b47da
RicardoPombinhaSantos <godhja@gmail.com>
2025-12-30 12:14:12 +0000

    Initialize git repository and push to remote

---
+++ init.sh
@@ -0,0 +1,6 @@
+git init
+git add .
+git commit -m "primeira versão chatbot guesthouse"
+git branch -M main
+git remote add origin https://github.com/teu-utilizador/guesthouse-chatbot.git
+git push -u origin main
